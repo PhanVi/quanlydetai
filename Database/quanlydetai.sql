3f11e1dbb39e2c3da8504188cc614334ec8c64cf
phan.vi <phanthituongvi96@gmail.com>
2020-12-22 22:57:08 +0700

    commit sql

--- Database/quanlydetai.sql
+++ Database/quanlydetai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 21, 2020 at 05:58 PM
+-- Generation Time: Dec 22, 2020 at 04:52 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
