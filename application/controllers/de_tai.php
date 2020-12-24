1fb8ab381fc856d3f6936ced8450de1983730aa8
NguyenMinhThien97 <minhthiennguyen1997@gmail.com>
2020-12-25 01:12:04 +0700

    upcode

--- application/controllers/de_tai.php
+++ application/controllers/de_tai.php
@@ -13,5 +13,22 @@
 		$this->load->view('layoutDeTai', $data );
 
 	 }
+	 public function dangKyDeTai($MaDeTai)
+	 {
+		$checkSlDK=$this->de_tai_table->checkSlDK($MaDeTai);
+		if($checkSlDK->result == "0"){
+			$MaSinhVien=$this->session->userdata("username");
+			$checkSVDK = $this->de_tai_table->checkSVDK($MaSinhVien);
+			if($checkSVDK == 0){
+				$this->de_tai_table->insertSvdk($MaDeTai, $MaSinhVien);
+				$this->de_tai_table->updateSoLuongDK($MaDeTai);
+				redirect('de_tai/danh_sach');
+			}else{
+				echo "<script language='javascript'>alert('bạn đã đăng ký đề tài nên không thể đăng ký thêm!'); document.location.href='https://localhost/quanlydetai/index.php/de_tai/danh_sach';</script>"; 
+			}
+		}else{
+			echo "<script language='javascript'>alert('Đề tài đã đủ số lượng đăng ký!'); document.location.href='https://localhost/quanlydetai/index.php/de_tai/danh_sach';</script>"; 
+		}
+	}
  }
 ?>
